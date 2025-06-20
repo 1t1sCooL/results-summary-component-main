a3cea70d75b2c75601512800b75f65d80a076e8e
1t1sCooL <mmalabugin@gmail.com>
2025-06-20 03:45:28 +0300

    edit styles

--- src/Results.tsx
+++ src/Results.tsx
@@ -2,17 +2,19 @@ function Results(){
     return(
         <>
             <div className='results'>
-          <h1 className='section-title'>
-            Your Result
-          </h1>
-          <p className='result-score'>
-            <span>76</span> of 100
-          </p>
-          <div className='grid-flow'>
-            <p className="result-rank">Great</p>
-            <p>You scored higher than 65% of the people who have taken these tests.</p>
-          </div>
-        </div>
+                <h1 className='section-title'>
+                    Your Result
+                </h1>
+                <p className='result-score'>
+                    <span>76</span> of 100
+                </p>
+                <div className='grid-flow'>
+                    <p className="result-rank">Great</p>
+                    <p>You scored higher than 65% of 
+                        the people who have taken these tests.
+                    </p>
+                </div>
+            </div>
         </>
     )
 }
